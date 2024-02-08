e877c3595774c6e32917caf3118bce94c51433f3
akashver20 <137759031+akashver20@users.noreply.github.com>
2024-02-08 22:56:19 +0530

    Add files via upload

---
+++ web3projectlinks-main/src/app/database/data.ts
@@ -0,0 +1,32 @@
+export const sideBarData = [
+    {
+        title: "Tags",
+        tags: ["pos",
+            "smart contracts",
+            "Polygon ecosystem",
+            "layer 2",
+            "eVM"]
+    },
+    {
+        title: "Coin Details",
+        tags: ["Polygon",
+            "MATIC",
+            "logo/polygon.png",
+            "∞"]
+    },
+    {
+        title: "Official Links",
+        tags: ["https://polygon.technology/",
+            "https://docs.polygon.technology/",
+            "https://github.com/maticnetwork/",
+            "https://forum.matic.network/",
+            "https://twitter.com/0xPolygon"]
+    },
+    {
+        title: "Social Platforms",
+        tags: ["https://discord.gg/polygon",
+            "https://twitter.com/0xPolygon",
+            "https://www.reddit.com/r/maticnetwork/",
+            "https://polygon.technology/community"]
+    },
+]
